ee4680ecd015d75efdcb559dd52877e4f1ffb222
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-02-23 16:43:29 +0300

    functional works. add logging for check_tokens

--- homework.py
+++ homework.py
@@ -1,10 +1,21 @@
+import logging
 import os
 import time
 import telegram
 import requests
 from telegram import Bot
 from dotenv import load_dotenv
+from logging import StreamHandler
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 load_dotenv()
 
@@ -25,7 +36,6 @@ HOMEWORK_STATUSES = {
 }
 
 
-bot = Bot(token=TELEGRAM_TOKEN)
 
 def send_message(bot, message):
     bot.send_message(
@@ -37,7 +47,7 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    hw_status = requests.get(ENDPOINT, HEADERS, params)
+    hw_status = requests.get(ENDPOINT, headers=HEADERS, params=params)
     return hw_status.json()
 
 
@@ -54,35 +64,44 @@ def parse_status(homework):
 
 def check_tokens():
     tokens_status = bool(PRACTICUM_TOKEN and TELEGRAM_CHAT_ID and TELEGRAM_TOKEN)
+    token_dict = {
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID
+    }
+    for token_name, token_value  in token_dict.items():
+        if not token_value:
+            logging.critical(f'Отсутствует обязательная переменная окружения: {token_name}')
     return tokens_status
 
 def main():
     """Основная логика работы бота."""
 
-    ...
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time()) - 60 * 60 * 24 * 40
 
-    ...
+    if check_tokens():
+        while True:
+            try:
+                response = get_api_answer(current_timestamp)
+                hw_list = check_response(response)
+                message = ''
+                for elem in hw_list:
+                    message += '\n' + parse_status(elem)
+                    print(message)
+                send_message(bot, message)
 
-    while True:
-        try:
-            response = ...
+                current_timestamp = time.time()
 
-            ...
+                time.sleep(RETRY_TIME)
 
-            current_timestamp = ...
-            print(check_tokens(), 'aaa')
-            time.sleep(RETRY_TIME)
-            break
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+            except Exception as error:
+                message = f'Сбой в работе программы: {error}'
+                ...
+                break
+                time.sleep(RETRY_TIME)
+            else:
+                break
 
 
 if __name__ == '__main__':
